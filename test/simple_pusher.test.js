312aaec6fd957246b2dbabd1b79d65267e544c1b
Andrew Donaldson <andrew@desto.net>
2011-05-20 15:15:54 +0100

    Restructure project, moved code into lib

---
+++ test/simple_pusher.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var simple_pusher = require('../index.js');
+
+module.exports = {
+  'test_something': function() {
+    //var config = {};
+    //var channel = 'test_channel';
+    //var event_name = 'test';
+    //var data = 'test';
+
+    //assert.ok(simple_pusher.trigger(config, channel, event_name, data));
+  }
+};
